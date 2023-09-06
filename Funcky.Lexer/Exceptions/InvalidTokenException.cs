1d123d55af77ffa4edeafb103d167942e6752faa
Thomas Bruderer <thomas.bruderer@messerli.ch>
2023-09-06 16:04:12 +0200

    Add LexemeWalkerExtensions

---
+++ Funcky.Lexer/Exceptions/InvalidTokenException.cs
@@ -0,0 +1,9 @@
+﻿namespace Funcky.Lexer.Exceptions;
+
+public sealed class InvalidTokenException : LexerException
+{
+    public InvalidTokenException(string message)
+        : base(message)
+    {
+    }
+}
---
+++ Funcky.Lexer/Extensions/LexmeWalkerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Funcky.Lexer.Exceptions;
+using Funcky.Lexer.Token;
+
+namespace Funcky.Lexer.Extensions;
+
+public static class LexmeWalkerExtensions
+{
+    public static Lexeme Consume<TToken>(this ILexemeWalker walker)
+        where TToken : IToken
+        => ConsumeLexeme<TToken>(walker.Pop());
+
+    public static bool NextIs<TType>(this ILexemeWalker walker, int lookAhead = 0)
+        => walker.Peek(lookAhead).Token is TType;
+
+    private static Lexeme ConsumeLexeme<TToken>(Lexeme lexeme)
+        where TToken : IToken
+        => lexeme is { Token: TToken }
+            ? lexeme
+            : HandleMissingLexeme<TToken>(lexeme);
+
+    private static Lexeme HandleMissingLexeme<TToken>(Lexeme lexeme)
+        => ThrowExpectingTokenException<TToken>(lexeme, lexeme.Position);
+
+    private static Lexeme ThrowExpectingTokenException<TToken>(Lexeme lexeme, Position position)
+        => throw new InvalidTokenException($"Expecting {typeof(TToken).FullName} but got {lexeme.Token} at Line {position.Line} Column {position.StartColumn}.");
+}
